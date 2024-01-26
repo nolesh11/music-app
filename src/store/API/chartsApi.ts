7ea0f2569e742d20b63de62640bb9911c685fd76
Aleksey Nogay <leshkanogay@gmail.com>
2024-01-26 20:31:10 +0500

    finished chartsApi

--- src/store/API/chartsApi.ts
+++ src/store/API/chartsApi.ts
@@ -14,14 +14,49 @@ export const chartApi = createApi({
   endpoints: (build) => ({
     getChartSongs: build.query<Response, any>({ // eslint-disable-line
       query: (params: [string, string]) => ({
-        url: 'chart/songs/',
+        url: "chart/songs/",
         params: {
           time_period: params[0],
           chart_genre: params[1],
-        }
-      })
-    })
+        },
+      }),
+    }),
+    getChartAlbums: build.query<Response, any>({ // eslint-disable-line
+      query: (timePeriod: string) => ({
+        url: "/chart/albums/",
+        params: {
+          time_period: timePeriod,
+        },
+      }),
+    }),
+    getChartArtist: build.query<Response, string>({
+      query: (timePeriod: string) => ({
+        url: "/chart/artists/",
+        params: {
+          time_period: timePeriod,
+        },
+      }),
+    }),
+    getChartLyrics: build.query<Response, string>({
+      query: (timePeriod: string) => ({
+        url: "/chart/referents/",
+        params: {
+          time_period: timePeriod,
+        },
+      }),
+    }),
+    getChartsSongsById: build.query<Response, string>({
+      query: (q: string) => ({
+        url: `chart/songs/${q}`,
+      }),
+    }),
   }),
-})
+});
 
-export const { useGetChartSongsQuery } = chartApi
+export const {
+  useGetChartSongsQuery,
+  useGetChartAlbumsQuery,
+  useGetChartArtistQuery,
+  useGetChartLyricsQuery,
+  useLazyGetChartsSongsByIdQuery,
+} = chartApi;
